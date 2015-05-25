d1add9cd410d065227b41c998517aedba03f5984
Mark Kamsma <mark@markkamsma.nl>
2015-05-25 14:11:58 +0200

    Improved the neighbor generation algorithm of TabuSearcher. Also removed some unused classes.
    Signed-off-by: Mark Kamsma <mark@markkamsma.nl>

--- Superdoku/LocalSearchers/TabuSearcher.cs
+++ Superdoku/LocalSearchers/TabuSearcher.cs
@@ -9,11 +9,16 @@ namespace Superdoku
     /// <summary>This class implements the tabu search technique.</summary>
     class TabuSearcher : LocalSearcher
     {
+        /// <summary>A list of all possible neighbors.</summary>
+        private List<SwapNeighbor> allNeighbors;
+
+        /// <summary>The neighbor we last applied.</summary>
+        private SwapNeighbor lastApplied;
+
         /// <summary>Constructor.</summary>
         /// <param name="maxIterations">The maximum amount of iterations the searcher should perform (negative value for unlimited).</param>
         public TabuSearcher(int maxIterations = -1)
             : base(maxIterations) { }
-
 
         /// <summary>Calculates the length of the tabu list for a sudoku of the given size.</summary>
         /// <param name="n">The size of the sudoku (n*n by n*n squares).</param>
@@ -28,13 +33,17 @@ namespace Superdoku
             // Initialise the best solution
             bestSolution = new LocalSudoku(sudoku);
 
+            // Reset the iterations
+            iterations = 0;
+
+            // Initialise the list of all neighbors
+            allNeighbors = generateNeighbors(sudoku);
+            lastApplied = null;
+
             // The tabu list
             int tabuListSize = tabuListLength(sudoku.N);
             HashSet<SwapNeighbor> tabuList = new HashSet<SwapNeighbor>();
             Queue<SwapNeighbor> tabuQueue = new Queue<SwapNeighbor>(tabuListSize);
-
-            // Reset the iterations
-            iterations = 0;
 
             // Keep running while the sudoku has not been solved yet (and we have not reached our iteration limit)
             while(sudoku.HeuristicValue > 0 && (maxIterations < 0 || iterations < maxIterations))
@@ -43,13 +52,7 @@ namespace Superdoku
                 ++iterations;
 
                 // Find the best neighbor that is not on the tabu list
-                List<SwapNeighbor> neighbors = generateNeighbors(sudoku, tabuList);
-                SwapNeighbor bestNeighbor = null;
-                foreach(SwapNeighbor neighbor in neighbors)
-                {
-                    if(bestNeighbor == null || neighbor.ScoreDelta < bestNeighbor.ScoreDelta)
-                        bestNeighbor = neighbor;
-                }
+                SwapNeighbor bestNeighbor = generateBestNeighbor(sudoku, tabuList);
 
                 // If no neighbor can be found, we stop the search process
                 if(bestNeighbor == null)
@@ -61,6 +64,7 @@ namespace Superdoku
                     tabuList.Remove(tabuQueue.Dequeue());
                 tabuList.Add(bestNeighbor);
                 tabuQueue.Enqueue(bestNeighbor);
+                lastApplied = bestNeighbor;
 
                 // Remember the best solution
                 if(sudoku.HeuristicValue < bestSolution.HeuristicValue)
@@ -70,59 +74,49 @@ namespace Superdoku
             return sudoku.HeuristicValue == 0;
         }
 
-        /// <summary>Generates the Neighbors of a LocalSudoku.</summary>
-        /// <param name="sudoku">The sudoku.</param>
-        /// <returns>A list containing neighbors.</returns>
-        protected List<SwapNeighbor> generateNeighbors(LocalSudoku sudoku, HashSet<SwapNeighbor> tabulist)
+        /// <summary>Generates the best neighbor for a given solution, keeping the tabu list in mind.</summary>
+        /// <param name="sudoku">The solution to generate a neighbor for.</param>
+        /// <returns>The best neighbor that could be found or null if no neighbor could be found.</returns>
+        private SwapNeighbor generateBestNeighbor(LocalSudoku sudoku, HashSet<SwapNeighbor> tabulist)
         {
             SudokuIndexHelper helper = SudokuIndexHelper.get(sudoku.N);
 
-            // Each box contains N^2 squares, so at most there will be N^2 + (N^2 - 1) + (N^2 - 2) + ... + 1 = N^2 (N^2 + 1) / 2 combinations possible.
-            // There are N^2 boxes, so that results in a capacity of N^2 * N^2 (N^2 + 1) / 2
-            List<SwapNeighbor> result = new List<SwapNeighbor>(sudoku.NN * sudoku.NN * (sudoku.NN + 1) / 2);
-            SwapNeighbor temp;
+            // The best neighbor we found so far
+            SwapNeighbor bestNeighbor = null;
 
-            // Loop through all boxes
-            for (int box = 0; box < sudoku.NN; ++box)
+            // Get the peers of the squares that were swapped in the last iteration
+            List<int> peers = new List<int>(2 * helper.getPeerCount() + 2);
+            if(lastApplied != null)
             {
-                // Within a box we loop through all squares and add all possibilities to the list
-                int[,] units = helper.getUnitsFor(sudoku.N * (box % sudoku.N), sudoku.N * (box / sudoku.N));
-                for (int square1 = 0; square1 < units.GetLength(1); ++square1)
+                peers.AddRange(helper.getPeersFor(lastApplied.Square1));
+                peers.Add(lastApplied.Square1);
+                peers.AddRange(helper.getPeersFor(lastApplied.Square2));
+                peers.Add(lastApplied.Square2);
+            }
+
+            // Loop through all neighbors
+            for(int neighbor = 0; neighbor < allNeighbors.Count; ++neighbor)
+            {
+                // Check if this neighbor needs to have its score delta updated
+                if(peers.Contains(allNeighbors[neighbor].Square1) || peers.Contains(allNeighbors[neighbor].Square2))
+                    allNeighbors[neighbor] = new SwapNeighbor(allNeighbors[neighbor].Square1, allNeighbors[neighbor].Square2, sudoku.heuristicDelta(allNeighbors[neighbor].Square1, allNeighbors[neighbor].Square2));
+
+                // If this neighbor is not on the tabu list, it is a candidate for a better neighbor
+                if(!tabulist.Contains(allNeighbors[neighbor]))
                 {
-                    if (sudoku.isFixed(units[SudokuIndexHelper.UNIT_BOX_INDEX, square1]))
-                        continue;
-
-
-                    for (int square2 = square1 + 1; square2 < units.GetLength(1); ++square2)
+                    if(bestNeighbor == null || allNeighbors[neighbor].ScoreDelta < bestNeighbor.ScoreDelta)
                     {
-                        if (!sudoku.isFixed(units[SudokuIndexHelper.UNIT_BOX_INDEX, square2]))
-                        {
-                          temp = new SwapNeighbor(units[SudokuIndexHelper.UNIT_BOX_INDEX, square1],
-                                                  units[SudokuIndexHelper.UNIT_BOX_INDEX, square2],
-                                                  0);
-                           
-                            //Checks wheter the tabulist contains the coordinates
-                            //This ensures that we call calculateHeuristicDelta less
-                            if(!tabulist.Contains(temp))
-                            {
-                                temp = new SwapNeighbor(
-                                    units[SudokuIndexHelper.UNIT_BOX_INDEX, square1],
-                                    units[SudokuIndexHelper.UNIT_BOX_INDEX, square2],
-                                    sudoku.heuristicDelta(units[SudokuIndexHelper.UNIT_BOX_INDEX, square1], units[SudokuIndexHelper.UNIT_BOX_INDEX, square2])
-                                );
-                                result.Add(temp);
-                                
-                                //We're never going to find a better score than -4, might as well return the score.
-                                if (temp.ScoreDelta == -4)
-                                    return new List<SwapNeighbor> { temp };
-                            }
-                        }
+                        bestNeighbor = allNeighbors[neighbor];
+
+                        // We will never be able to improve a score delta of -4
+                        if(bestNeighbor.ScoreDelta == -4)
+                            return bestNeighbor;
                     }
                 }
             }
 
-            // Return the list
-            return result;
+            // Return the best neighbor
+            return bestNeighbor;
         }
     }
 }
